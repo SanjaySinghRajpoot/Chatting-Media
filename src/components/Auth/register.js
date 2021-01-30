d0fc8a96f580a7dc29968bb69fdb01c8d34c4fcf
sanjay rajpoot <iamsanjayrajpoot@gmail.com>
2021-01-30 21:59:50 +0530

    Login form back end created

--- src/components/Auth/register.js
+++ src/components/Auth/register.js
@@ -1,35 +1,106 @@
-import React from 'react';
-import {Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import firebase from '../../firebase';
+ 
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon,
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
+class Register extends React.Component {
+  state = {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirmation: ''
+  };
 
- class Register extends React.Component {
-     state = {}
-     handleChange = () => {}
-   render(){
-       return(
-           <Grid textAlign="center" verticalAlign="middle" className="app">
-               <Grid.Column style={{ maxWidth: 450}}>
-                   <Header as="h2" icon color="orange" textAlign="center" > 
-                        <Icon name="puzzle piece" color="orange"/>
-                        Register for Nut Chat 
-                   </Header>
-                <Form  size="large">
-                     <Segment stacked>
-                         <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="username" onChange={this.handleChange} type="text" />
-                         <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email" onChange={this.handleChange} type="email" />
-                         <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="password" onChange={this.handleChange} type="password" />
-                         <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="password" onChange={this.handleChange} type="password" />
-                         <Button color="orange" fluid size ="large">Submit</Button>
-                     </Segment>
-                </Form>
-                  <Message>
-                      Already A user? <Link to="/login">login</Link>
-                  </Message>
-               </Grid.Column>
-           </Grid>
-       )
-   }
- }
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value});
+  };
 
- export default Register;
+  handleSubmit = event =>{
+        event.preventDefault();
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(this.state.email, this.state.password)
+          .then(createdUser => {
+            console.log(createdUser);
+          })
+          .catch(err => {
+              console.error(err);
+          });
+  };
+
+  render() {
+    const {username, email, password, passwordConfirmation} = this.state;
+
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for Nut Chat
+          </Header>
+          <Form onSubmit={this.handleSubmit} size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="username"
+                onChange={this.handleChange}
+                value={username}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email"
+                onChange={this.handleChange}
+                type="email"
+                value={email}
+              />
+              <Form.Input
+                fluid
+                name="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="password"
+                onChange={this.handleChange}
+                type="password"
+                value={password}
+              />
+              <Form.Input
+                fluid
+                name="passwordConfirmation"
+                icon="repeat"
+                iconPosition="left"
+                placeholder="Password Confirmation"
+                onChange={this.handleChange}
+                type="password"
+                value={passwordConfirmation}
+              />
+              <Button color="orange" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already A user? <Link to="/login"> Login </Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default Register;
