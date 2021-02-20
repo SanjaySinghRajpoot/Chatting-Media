542aa2b7a5d1c549016ce1c8f0d331358471d946
sanjay rajpoot <iamsanjayrajpoot@gmail.com>
2021-02-20 15:16:48 +0530

    Messages Tab Created with backend

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {sidebar, Menu, Divider, Button, Sidebar} from 'semantic-ui-react';
 
 class ColorPanel extends React.Component {
     render(){
         return(
-            <div> ColorPanel </div>
-        )
+           <Sidebar
+             as={Menu}
+             icon= "labeled"
+             inverted
+             vertical
+             visible
+             width= "very thin"
+           >
+
+               <Divider />
+               <Button icon="add" size="small" color="blue"/>
+
+           </Sidebar>
+        );
     }
 }
 
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import firebase from "../../firebase";
+
+import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessagesForm";
 
 class Messages extends React.Component {
-    render(){
-        return(
-            <div> Messages </div>
-        )
-    }
-}
+  state = {
+    messagesRef: firebase.database().ref("messages"),  //DATA is sent to firebase From MessageForm
+    channel: this.props.currentChannel,
+    user: this.props.currentUser
+  };
 
+  render() {
+    const { messagesRef, channel, user } = this.state;
+
+    return (
+      <React.Fragment>
+        <MessagesHeader />
+        <Segment>
+          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+        </Segment>
+
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+        />
+      </React.Fragment>
+    );
+  }
+}
 
 export default Messages;
