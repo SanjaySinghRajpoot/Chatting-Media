942812b13bd9806c6c105d060555fd867df3c96d
sanjay <iamsanjayrajpoot@gmail.com>
2021-05-24 21:34:46 +0530

    Metapanel working

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,8 +1,31 @@
 import React from "react";
+import {Header, Accordion, Icon, Segment } from "semantic-ui-react"
 
 class MetaPanel extends React.Component {
+ 
+   state  = {
+     activeIndex : 0,
+     
+
+   }
+ 
+ 
   render() {
-    return <div>MetaPanel</div>;
+    return (
+      <Segment>
+        <Header as="h3" attached="top">
+           About # Channel
+        </Header>
+        <Accordion styled attached="true">
+          <Accordion.Title 
+           active={activeIndex === 0}
+           index = {0} 
+           >
+           </Accordion.Title>
+
+        </Accordion>
+      </Segment>
+    );
   }
 }
 
