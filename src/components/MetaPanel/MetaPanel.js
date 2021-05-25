df478f8267fa9cefabca197c3cbd7caf672dfdf6
sanjay <iamsanjayrajpoot@gmail.com>
2021-05-25 22:06:51 +0530

    Metapanel created backed left

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -5,12 +5,20 @@ class MetaPanel extends React.Component {
  
    state  = {
      activeIndex : 0,
-     
+   }
 
+   setActiveIndex = (event, titleProps) =>{
+    const { index } = this.state;
+    const { activeIndex } = this.state;
+
+     const newIndex  = activeIndex === index ? -1 : index;
+     this.setState({activeIndex: newIndex}); 
    }
  
  
   render() {
+    const { activeIndex } = this.state;
+
     return (
       <Segment>
         <Header as="h3" attached="top">
@@ -18,10 +26,44 @@ class MetaPanel extends React.Component {
         </Header>
         <Accordion styled attached="true">
           <Accordion.Title 
-           active={activeIndex === 0}
+           active = {activeIndex === 0}
            index = {0} 
+           onClick = {this.setActiveIndex}
            >
-           </Accordion.Title>
+             <Icon name = "dropdown"/>
+             <Icon name = "info" />
+             Channel Deatils
+            </Accordion.Title>
+            <Accordion.Content active={activeIndex === 0}>
+
+            </Accordion.Content>
+
+            <Accordion.Title 
+           active = {activeIndex === 1}
+           index = {1} 
+           onClick = {this.setActiveIndex}
+           >
+             <Icon name = "dropdown"/>
+             <Icon name = "user circle" />
+             Top Posters
+            </Accordion.Title>
+            <Accordion.Content active={activeIndex === 1}>
+              Posters
+            </Accordion.Content>
+
+            <Accordion.Title 
+           active = {activeIndex === 2}
+           index = {2} 
+           onClick = {this.setActiveIndex}
+           >
+             <Icon name = "dropdown"/>
+             <Icon name = "pencil alternate" />
+             Created By
+            </Accordion.Title>
+            <Accordion.Content active={activeIndex === 2}>
+                Creator
+            </Accordion.Content>
+
 
         </Accordion>
       </Segment>
